20140fc790ae7bfe90ee3a0390a8c2282f2632ad
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-11-24 11:16:06 +0100

    server listens on port 3001 for incoming requests to / route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+}
 
 func main() {
-	fmt.Println("running")
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 3001, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production)")
+	flag.Parse()
+
+	app := &application{
+		config: cfg,
+	}
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal("ERROR:", err)
+	}
 }
---
+++ cmd/api/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() http.Handler {
+	router := httprouter.New()
+
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		fmt.Fprint(w, "home page")
+	})
+
+	return router
+}
