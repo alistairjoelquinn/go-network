8a4e992ac2882539435e47e32cf05340de9439fc
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-12-07 14:06:04 +0100

    refactors main.go, moves in static files o server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,42 +1,32 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 )
 
-type config struct {
-	port int
-	env  string
-}
-
 type application struct {
-	config config
+	port int
 }
 
 func main() {
-	var cfg config
-
-	flag.IntVar(&cfg.port, "port", 3001, "Server port to listen on")
-	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production)")
-	flag.Parse()
-
 	app := &application{
-		config: cfg,
+		port: 3001,
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.port),
 		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  2 * time.Minute,
+		ReadTimeout:  6 * time.Second,
+		WriteTimeout: 20 * time.Second,
 	}
 
-	err := srv.ListenAndServe()
+	log.Println("Server running on port", app.port)
+
+	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal("ERROR:", err)
 	}
