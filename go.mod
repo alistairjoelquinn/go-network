8aa644f214f6fe62e669d483a9f5b67db2468440
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-12-14 10:13:20 +0100

    rename util

--- go.mod
+++ go.mod
@@ -12,7 +12,9 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/aws/aws-sdk-go v1.42.23 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/klauspost/compress v1.13.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
--- util/upload.go
+++ util/upload.go
@@ -1,1 +1,32 @@
 package util
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+func UploadImage(filename string) error {
+	sess := session.Must(session.NewSession())
+	uploader := s3manager.NewUploader(sess)
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("failed to open file %q, %v", filename, err)
+	}
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(Env("AWS_BUCKET")),
+		Key:    aws.String(Env("AWS_KEY")),
+		Body:   f,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to upload file, %v", err)
+	}
+	fmt.Printf("file uploaded to, %s\n", filename)
+
+	return nil
+}
