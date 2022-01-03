bfd81a44a50441519a2be26c89010284cb8844d4
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-01-03 15:45:48 +0100

    removes old logs

--- handler/friendship-handler.go
+++ handler/friendship-handler.go
@@ -39,9 +39,7 @@ func GetFriendsList(c *fiber.Ctx) error {
 		return c.SendStatus(500)
 	}
 	if len(*users) == 0 {
-		log.Println("no friends")
 		return c.JSON(users)
 	}
-	log.Println("friends", users)
 	return c.JSON(users)
 }
